be9bcb3cec383cc8a041a6790ad94c41eff0d53b
KIM MINSU <112890661+chunjaeilu@users.noreply.github.com>
2022-11-21 17:48:35 +0900

    11.21 피드백 수정
    - 서브페이지 링크 수정(없는 페이지 링크 없앰)
    - sub4-1 예약 팝업 구현

--- js/sub.js
+++ js/sub.js
@@ -22,8 +22,31 @@ $(function () {
 /* #sub4-1 버튼 전환 효과 */
 
 $(function () {
+  let btnHallList = ["홀I", "홀IV", "홀V", "홀402"];
   $("#sub4-1 .section3 button").on("click", function () {
     $("#sub4-1 .section3 button").removeClass("active");
     $(this).addClass("active");
+    btnNum = $(this).index();
+    $("#sub4-1 .section4 .popup-reservation .choosed-hall").html(
+      btnHallList[btnNum]
+    );
+  });
+});
+
+/* #sub4-1 연습실 예약 */
+/* 달력에서 날짜 선택 */
+$(function () {
+  $("#sub4-1 .section4 .calendar-box .calendar td").on("click", function () {
+    $("#sub4-1 .section4 .calendar-box .calendar td").removeClass("choosed");
+    $(this).addClass("choosed");
+    $("#sub4-1 .section4 .popup-reservation").addClass("view");
+
+    /* 선택날짜 팝업창 표시 */
+    choosedDate = "2022.11." + $(this).html();
+    $("#sub4-1 .section4 .popup-reservation h3").html(choosedDate);
+  });
+
+  $("#sub4-1 .section4 .popup-reservation .btn-close").on("click", function () {
+    $("#sub4-1 .section4 .popup-reservation").removeClass("view");
   });
 });
